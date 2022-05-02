8861998bf75dedb0211d2db6da0a10e061444b6f
westsara <sara.westling@student.tabyenskilda.se>
2022-05-02 14:13:44 +0200

    Page 1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-/* Side navigation menu*/
+/* Navigeringsmeny*/
 
-/* Set the width of the side navigation to 20vw */
+/* Bredden av menyn ska vara 20vw */
 function openNav() {
   document.getElementById("mySidenav").style.width = "20vw";
 }
 
-/* Set the width of the side navigation to 0 */
+/* Bredden av menyn ska vara 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+}
+
+
+/*Bildspel*/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); /* Byt bild varannan sekund*/
 }
